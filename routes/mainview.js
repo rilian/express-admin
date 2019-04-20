fe1f3a4ad1e047137992f0c348b2344b2d87e59a
Osric Wong <osric.wong@introv.com>
2019-04-20 23:36:14 +0800

    user session checking

--- routes/mainview.js
+++ routes/mainview.js
@@ -18,10 +18,22 @@ exports.get = function (req, res, next) {
     }
 
     var customs = [];
-    for (var key in custom) {
-        var item = custom[key].app;
-        if (!item || !item.mainview || !item.mainview.show) continue;
-        customs.push({slug: item.slug, name: item.verbose});
+    if (req.session.user != null && req.session.user.id) {
+        for (var key in custom) {
+            var item = custom[key].app;
+            if (!item || !item.mainview || !item.mainview.show) continue;
+            if (item.mainview != null && item.mainview.users != null) {
+                var hasRight = false;
+                for (const u of item.mainview.users) {
+                    if (u == req.session.user.id) {
+                        hasRight = true;
+                        break;
+                    }
+                }
+                if (!hasRight) continue;
+            }
+            customs.push({slug: item.slug, name: item.verbose});
+        }
     }
 
     res.locals.tables = !tables.length ? null : {items: tables};
